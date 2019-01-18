f7b8e0eec1667a5ccea5bc740bff5aa3a06cc80f
corkili <corkili@163.com>
2019-01-18 20:07:29 +0800

    #2 完善ZipUtils

--- learningserver-scorm/src/main/java/com/corkili/learningserver/scorm/cam/load/ZipUtils.java
+++ learningserver-scorm/src/main/java/com/corkili/learningserver/scorm/cam/load/ZipUtils.java
@@ -1,30 +1,62 @@
 package com.corkili.learningserver.scorm.cam.load;
 
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class ZipUtils {
 
     private static final int BUFFER_SIZE = 1024;
 
-    public static boolean compressZip(File[] files, String zipFilePath) {
-        if (files == null || files.length <= 0) {
-            return false;
-        }
+    /**
+     * 未进行测试
+     */
+    public static boolean compressZip(String srcPath, String zipFilePath) {
         if (!isEndWithZip(zipFilePath)) {
             zipFilePath += ".zip";
         }
-        File zipFile = new File(zipFilePath);
-        // TODO 压缩方法
+        File srcFile = new File(srcPath);
+        List<File> fileList = getAllFiles(srcFile);
+        byte[] buffer = new byte[BUFFER_SIZE];
+        ZipEntry zipEntry;
+        int readLen;
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFilePath))){
+            for (File file : fileList) {
+                if (file.isFile()) {
+                    zipEntry = new ZipEntry(getRelativePath(srcPath, file));
+                    zipEntry.setSize(file.length());
+                    zipEntry.setTime(file.lastModified());
+                    zipOutputStream.putNextEntry(zipEntry);
+
+                    InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+                    while ((readLen = inputStream.read(buffer, 0, BUFFER_SIZE)) != -1) {
+                        zipOutputStream.write(buffer, 0, readLen);
+                    }
+
+                    inputStream.close();
+                } else {
+                    zipEntry = new ZipEntry(getRelativePath(srcPath, file) + "/");
+                    zipOutputStream.putNextEntry(zipEntry);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
         return true;
     }
 
@@ -41,7 +73,7 @@ public class ZipUtils {
         try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFilePath))) {
             ZipEntry zipEntry;
             byte[] buffer = new byte[BUFFER_SIZE];
-            int readLen = 0;
+            int readLen;
 
             while ((zipEntry = zipInputStream.getNextEntry()) != null) {
                 if (zipEntry.isDirectory()) {
